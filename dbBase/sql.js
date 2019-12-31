470bf76c091048fe0a5cc13507d1407de8ec56ad
lws <wensheng@yougola.com>
2019-12-31 18:01:21 +0800

    'note'

--- dbBase/sql.js
+++ dbBase/sql.js
@@ -1,6 +1,14 @@
 const sql = {
   // user
   loginSql: 'select * from account where openid = ?',
-  registerSql: 'insert into account (openid) values (?)'
+  registerSql: 'insert into account (openid) values (?)',
+  getNoteListSql: 'select * from note_list where openid = ?',
+  getNoteListAllSql: 'select * from note_list where type=public order by noteTime desc limit ?, ?',
+  addNoteSql: 'insert into note_list (openid, content, type, nickname) values (?,?,?,?)',
+  updateImagesSql: 'update note_list set images=? where noteId = ?',
+  deleteNoteSql: 'delete from note_list where noteId=?',
+  deleteNoteCollectSql: 'delete from collect_note where noteId=?',
+  collectNoteSql: 'insert into collect_note (noteId, openid) values (?,?)',
+  cancenCollectNoteSql: 'delete from collect_note where collectId = ?'
 }
 module.exports = sql
--- routes/note.js
+++ routes/note.js
@@ -1,9 +1,113 @@
 var express = require('express');
 var router = express.Router();
+const conn = require('../utils/dbUtils.js');
+const sql = require('../dbBase/sql.js');
+const multer  = require('multer');
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/note-list', function(req, res, next) {
+  let query = req.body
+  conn.query(sql.getNoteListSql, query.openid, (error, results) => {
+    if (error) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '获取失败'
+      })
+    }
+    return res.json({
+      data: results,
+      resultCode: 200
+    })
+  })
+})
+
+router.post('/note-list-all', function(req, res, next) {
+  let query = req.body
+  conn.query(sql.getNoteListAllSql, [query.start, query.length], (error, results) => {
+    if (error) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '获取失败'
+      })
+    }
+    return res.json({
+      data: results,
+      resultCode: 200
+    })
+  })
+})
+
+router.post('/addNote', function(req, res, next) {
+  let query = req.body
+  const { openid, content, type, username, images } = query
+  conn.query(sql.addNoteSql, [openid, content, type, nickname, images], (error, reuslts) => {
+    if (error) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '添加失败'
+      })
+    }
+    if (results) {
+      return res.json({
+        resultCode: 200,
+        noteId: results.insertId
+      })
+    } else {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '添加失败'
+      })
+    }
+  })
+})
+
+var upload = multer({
+  dest: 'uploads/images'
+})
+router.post('/upload', upload.single('Filedata'), function(req, res, next) {
+  let file = req.file;
+  let query = req.query
+  if (!file) {
+    return res.json({
+      resultCode: 5000
+    })
+  }
+  if (!query.noteId) {
+    return res.json({
+      resultCode: 5000
+    })
+  }
+  var oldfliepath = path.join('./',"uploads/images",file.filename)
+  var newfileName = file.filename + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]
+  var newfilepath = path.join('./',"uploads/images", newfileName)
+  fs.rename(oldfliepath,newfilepath,function(err){
+    if (err) throw err;
+    fs.stat(newfilepath, function (err, stats) {
+        if (err) throw err;
+      });
+  });
+  conn.query(sql.updateImagesSql, [newfileName, query.noteId], (err, results) => {
+    if (err) {
+      res.json({
+        resultCode: 5000
+      })
+    }
+    if (results) {
+      return res.json({
+        resultCode: 200
+      })
+    } else {
+      return res.json({
+        resultCode: 5000
+      })
+    }
+  })
+})
 
 module.exports = router;
